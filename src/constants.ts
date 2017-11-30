73bac3123026ecc1ff55100b8fe1ae75c60fbeef
Felix Rilling <contact@f-rilling.com>
2017-11-30 13:52:23 +0100

    improved constant ts file

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,8 @@
 /*
  * Using this can really reduce the minified output but can have performance and optimization issues
  */
-
-/*
-const _Number = Number;
+/* tslint:disable */
+/* const _Number = Number;
 const _Boolean = Boolean;
 const _String = String;
 const _Symbol = Symbol;
@@ -12,6 +11,19 @@ const _Object = Object;
 const _Array = Array;
 const _Set = Set;
 const _Map = Map;
+
+const _null = null;
+
+const _str_undefined = "undefined";
+const _str_string = "string";
+const _str_number = "number";
+const _str_boolean = "boolean";
+const _str_symbol = "symbol";
+const _str_object = "object";
+const _str_function = "function";
+
+const _str_length = "length";
+const _str_size = "size";
 
 export {
     _Number,
@@ -22,5 +34,17 @@ export {
     _Array,
     _Set,
     _Map,
-};
- */
+
+    _null,
+
+    _str_undefined,
+    _str_string,
+    _str_number,
+    _str_boolean,
+    _str_symbol,
+    _str_object,
+    _str_function,
+
+    _str_length,
+    _str_size,
+}; */
