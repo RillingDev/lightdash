6b6a091ef38611c8cbb8d5bb770d3617a4f7b4a5
Felix Rilling <felix@f-rilling.com>
2017-12-29 11:08:54 -0500

    deleted outdated typedefs

--- types/lib/add.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Add two numbers
- *
- * @param {number} a
- * @param {number} b
- * @returns {number}
- */
-declare const add: (a: number, b: number) => number;
-export default add;
--- types/lib/addPi.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Add Pi to a number
- *
- * @param {number} a
- * @returns {number}
- */
-declare const addPi: (a: number) => number;
-export default addPi;
