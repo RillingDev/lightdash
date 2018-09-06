8e26ce4d44eaace08f576be35f7933f2746124fc
Felix Rilling <felix@f-rilling.com>
2018-09-06 20:04:24 +0200

    Added tests for search and string.

--- spec/main.ts
+++ spec/main.ts
@@ -1,15 +1,15 @@
 import "./arr/chunk.spec";
-import "./arr/collect.spec";
 import "./arr/compact.spec";
-import "./arr/count.spec";
+import "./arr/step.spec";
 import "./arr/difference.spec";
-import "./arr/fromDeep.spec";
 import "./arr/intersection.spec";
-import "./arr/mapDeep.spec";
+import "./arr/uniq.spec";
+import "./arr/count.spec";
+import "./arr/collect.spec";
 import "./arr/removeIndex.spec";
 import "./arr/removeItem.spec";
-import "./arr/step.spec";
-import "./arr/uniq.spec";
+import "./arr/mapDeep.spec";
+import "./arr/fromDeep.spec";
 
 import "./for/eachDeep.spec";
 import "./for/eachEntry.spec";
@@ -30,3 +30,11 @@ import "./is/objectPlain.spec";
 
 import "./num/clamp.spec";
 import "./num/safe.spec";
+
+import "./search/binary.spec";
+
+import "./str/distance.spec";
+import "./str/similar.spec";
+import "./str/fromPascalCase.spec";
+import "./str/toPascalCase.spec";
+import "./str/toCamelCase.spec";
---
+++ spec/str/similar.spec.ts
@@ -0,0 +1,15 @@
+import { strSimilar } from "../../src/str/similar";
+
+describe("strSimilar", () => {
+    it("gets similar", () => {
+        expect(strSimilar("Fob", ["Foo", "Bar"])).toEqual(["Foo"]);
+        expect(strSimilar("cmmit", ["init", "commit", "push"])).toEqual(["commit"]);
+        expect(strSimilar("Kitten", ["Sitten", "Sitting", "Bitten"])).toEqual(["Sitten", "Bitten"]);
+        expect(strSimilar("cmmit", ["init", "commit", "push"], true)).toEqual(
+            new Map([
+                [1, ["commit"]],
+                [3, ["init"]],
+                [5, ["push"]]
+            ]));
+    });
+});
