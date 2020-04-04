0c8ffc6e7cf8828687fa7e89b82b938a951b287f
FelixRilling <contact@rilling.dev>
2020-04-04 13:13:28 +0200

    Directly export from main.

--- src/main.ts
+++ src/main.ts
@@ -1,44 +1,23 @@
-import { isBlank } from "./is/isBlank";
-import { isPromise } from "./is/isPromise";
+export { isBlank } from "./is/isBlank";
+export { isPromise } from "./is/isPromise";
 
-import { toMap } from "./lang/toMap";
+export { toMap } from "./lang/toMap";
 
-import { distance } from "./string/distance";
-import { pascalCase } from "./string/pascalCase";
-import { similar } from "./string/similar";
-import { matchAll } from "./string/matchAll";
+export { distance } from "./string/distance";
+export { pascalCase } from "./string/pascalCase";
+export { similar } from "./string/similar";
+export { matchAll } from "./string/matchAll";
 
-import { groupMapBy } from "./array/groupMapBy";
-import { groupMapReducingBy } from "./array/groupMapReducingBy";
-import { countMapBy } from "./array/countMapBy";
-import { removeIndex } from "./array/removeIndex";
-import { removeItem } from "./array/removeItem";
-import { step } from "./array/step";
+export { groupMapBy } from "./array/groupMapBy";
+export { groupMapReducingBy } from "./array/groupMapReducingBy";
+export { countMapBy } from "./array/countMapBy";
+export { removeIndex } from "./array/removeIndex";
+export { removeItem } from "./array/removeItem";
+export { step } from "./array/step";
 
-import { decycle } from "./object/decycle";
-import { name } from "./object/name";
-import { deepFreeze } from "./object/deepFreeze";
-import { deepSeal } from "./object/deepSeal";
+export { decycle } from "./object/decycle";
+export { name } from "./object/name";
+export { deepFreeze } from "./object/deepFreeze";
+export { deepSeal } from "./object/deepSeal";
 
-import { binarySearch } from "./search/binarySearch";
-
-export {
-    isPromise,
-    toMap,
-    pascalCase,
-    distance,
-    similar,
-    matchAll,
-    isBlank,
-    step,
-    removeIndex,
-    removeItem,
-    groupMapBy,
-    groupMapReducingBy,
-    countMapBy,
-    decycle,
-    deepFreeze,
-    deepSeal,
-    name,
-    binarySearch,
-};
+export { binarySearch } from "./search/binarySearch";
