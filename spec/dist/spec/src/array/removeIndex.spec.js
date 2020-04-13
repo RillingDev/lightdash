0d695d809db650926a5f91869c2612b06936ed5c
FelixRilling <contact@rilling.dev>
2020-04-13 10:43:32 +0200

    Output sourcemap in tests, remove test output from git.

--- spec/dist/spec/src/array/removeIndex.spec.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const removeIndex_1 = require("../../../src/array/removeIndex");
-describe("removeIndex", () => {
-    it("removes index", () => {
-        expect(removeIndex_1.removeIndex(["foo", "bar", "fizz"], 1)).toEqual(["foo", "fizz"]);
-    });
-});
--- spec/dist/spec/src/array/step.spec.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const step_1 = require("../../../src/array/step");
-describe("step", () => {
-    it("steps items", () => {
-        expect(step_1.step([1, 2, 3, 4, 5, 6], 2)).toEqual([1, 3, 5]);
-    });
-});
