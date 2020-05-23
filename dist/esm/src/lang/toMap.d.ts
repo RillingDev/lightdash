ead9329b040baa59a3afb5a663aba28533f5362f
FelixRilling <contact@rilling.dev>
2020-05-23 14:10:16 +0200

    Updated deps.

--- dist/esm/src/lang/toMap.d.ts
+++ dist/esm/src/lang/toMap.d.ts
@@ -15,6 +15,6 @@ import { Dictionary, NumericDictionary } from "lodash";
  * toMap({a: 1, b: 4, c: 5}, key => { return { key }; }, value => value * 2)
  * // => Map{{key: "a"}: 2, {key: "b"}: 8, {key: "a"}: 10}
  */
-declare const toMap: <TValue, UKey, VInitialValue>(object: Dictionary<VInitialValue> | NumericDictionary<VInitialValue>, keyMapper?: (key: string | number | symbol) => UKey, valueMapper?: (value: VInitialValue) => TValue) => Map<UKey, TValue>;
+declare const toMap: <TValue, UKey, VInitialValue>(object: Dictionary<VInitialValue> | NumericDictionary<VInitialValue>, keyMapper?: (key: PropertyKey) => UKey, valueMapper?: (value: VInitialValue) => TValue) => Map<UKey, TValue>;
 export { toMap };
 //# sourceMappingURL=toMap.d.ts.map
