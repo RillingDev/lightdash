63575ea0cbc18936b6d256a4e3ade595e53ac788
FelixRilling <contact@rilling.dev>
2020-06-05 19:33:47 +0200

    Fixed test suite name.

--- spec/src/string/pascalCase.spec.ts
+++ spec/src/string/pascalCase.spec.ts
@@ -1,6 +1,6 @@
 import { pascalCase } from "../../../src/string/pascalCase";
 
-describe("strToPascalCase", () => {
+describe("pascalCase", () => {
     it("converts to PascalCase", () => {
         expect(pascalCase("fooBar")).toBe("FooBar");
         expect(pascalCase("__fizz_buzz__")).toBe("FizzBuzz");
