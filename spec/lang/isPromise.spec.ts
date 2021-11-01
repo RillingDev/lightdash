f1555471992b37ead9b31b3c6c143a933ac34cf2
FelixRilling <contact@rilling.dev>
2021-11-01 13:55:54 +0100

    Added isPromise test.

---
+++ spec/lang/isPromise.spec.ts
@@ -0,0 +1,20 @@
+import { isPromise } from "../../src/lang/isPromise";
+
+describe("isPromise", () => {
+	it("true for promise", () => {
+		expect(
+			isPromise(
+				new Promise((resolve) => {
+					resolve("foo");
+				})
+			)
+		).toBeTrue();
+	});
+	it("false for other", () => {
+		expect(isPromise(() => "foo")).toBeFalse();
+		expect(isPromise({})).toBeFalse();
+		expect(isPromise([])).toBeFalse();
+		expect(isPromise(null)).toBeFalse();
+		expect(isPromise(undefined)).toBeFalse();
+	});
+});
