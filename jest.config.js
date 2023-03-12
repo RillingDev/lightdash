b1e35983468bc94e010c23734e6578021cad53a9
FelixRilling <contact@rilling.dev>
2023-03-12 10:03:00 +0100

    Updated stack.

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,13 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
+	// https://kulshekhar.github.io/ts-jest/docs/guides/esm-support/#use-esm-presets
 	preset: "ts-jest/presets/default-esm",
 	testEnvironment: "node",
-	// https://kulshekhar.github.io/ts-jest/docs/guides/esm-support/
 	moduleNameMapper: {
 		"^(\\.{1,2}/.*)\\.js$": "$1",
 	},
 	transform: {
-		"<regex_match_files>": [
+		"^.+\\.tsx?$": [
 			"ts-jest",
 			{
 				tsconfig: "./__tests__/tsconfig.json",
